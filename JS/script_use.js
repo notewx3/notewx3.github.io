6714c0130dc8c647a5be7019379c562885d0c66a
notecx3 <notenotewx3@gmail.com>
2024-06-06 19:57:04 +0800

    mmm

--- JS/script_use.js
+++ JS/script_use.js
@@ -1,4 +1,30 @@
 
+//menu改block01
+
+function toggleMenu() {
+    var block01 = document.getElementById("block01"); // 獲取菜單元素
+    var hamburger = document.querySelector(".hamburger"); // 獲取漢堡按鈕
+    if (block01.style.right === "0px") {
+        block01.style.right = "-250px"; // 如果菜單欄已經顯示，則將其滑回屏幕右側外
+        hamburger.style.display = "block"; // 顯示漢堡按鈕
+    } else {
+        block01.style.right = "0px"; // 如果菜單欄隱藏，則將其滑出到屏幕內
+        hamburger.style.display = "none"; // 隱藏漢堡按鈕
+    }
+}
+
+function showMessage() {
+    var block01 = document.getElementById("block01"); // 獲取菜單元素
+    var message = document.getElementById("message"); // 獲取顯示信息的元素
+    var hamburger = document.querySelector(".hamburger"); // 獲取漢堡按鈕
+    block01.style.right = "-250px"; // 將菜單欄滑回屏幕右側外
+    message.style.display = "block"; // 顯示信息
+    hamburger.style.display = "block"; // 顯示漢堡按鈕
+    setTimeout(function() {
+        message.style.display = "none"; // 設置一個兩秒後自動隱藏信息的計時器
+    }, 2000);
+}
+
 document.getElementById('show-dialog').addEventListener('click', function() {
     console.log('觸發按鈕');
     const dialog = document.getElementById('dialog');
